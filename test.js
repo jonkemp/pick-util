27d63387c00478688e729ffc3f4c0cd49a931d98
Jonathan Kemp <kempdogg@gmail.com>
2020-02-19 23:24:58 -0500

    Update test cases.

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 const assert = require('assert');
 const pick = require('./');
 
+const isNumber = obj => toString.call(obj) === '[object Number]';
+
 describe('pick', () => {
 	it('should look through the list and return a copy of the object only containing the whitelisted properties', () => {
-		assert.deepEqual(pick({ 'a': 1, 'b': '2', 'c': 3 }, 'a', 'c'), { 'a': 1, 'c': 3 });
-		assert.deepEqual(pick({ 'user': 'barney',  'age': 36, 'active': true }, ['user', 'active']), { 'user': 'barney', 'active': true });
+		assert.deepEqual(pick({ 'a': 1, 'b': '2', 'c': 3 }, ['a', 'c']), { 'a': 1, 'c': 3 });
+		assert.deepEqual(pick({name: 'moe', age: 50, userid: 'moe1'}, 'name', 'age'), { name: 'moe', age: 50 });
+		assert.deepEqual(pick({name: 'moe', age: 50, userid: 'moe1'}, (value) => isNumber(value)), { age: 50 });
 	});
 });
