3eee8548f4bddbd7e1702de70ac5f157a9f4a4fb
Jonathan Kemp <kempdogg@gmail.com>
2020-03-21 12:50:47 -0400

    Should work with a predicate and context
    and include prototype props.

--- index.js
+++ index.js
@@ -2,18 +2,46 @@ const flatten = require('flat-util');
 
 const isFunction = obj => toString.call(obj) === '[object Function]';
 
+const optimizeCb = (func, context, argCount) => {
+	if (context === void 0) return func;
+	switch (argCount == null ? 3 : argCount) {
+		case 1: return value => func.call(context, value);
+			// The 2-argument case is omitted because we’re not using it.
+		case 3: return (value, index, collection) => func.call(context, value, index, collection);
+		case 4: return (accumulator, value, index, collection) => func.call(context, accumulator, value, index, collection);
+	}
+
+	return (...args) => func.apply(context, args);
+};
+
+const isObject = obj => {
+	const type = typeof obj;
+
+	return type === 'function' || type === 'object' && !!obj;
+};
+
+const allKeys = obj => {
+	if (!isObject(obj)) return [];
+	const keys = [];
+
+	for (const key in obj) keys.push(key);
+
+	return keys;
+};
+
 const keyInObj = (value, key, obj) => key in obj;
 
 module.exports = (obj, ...keys) => {
 	const result = {};
-	let iteratee = keys[0];
+	let [iteratee] = keys;
 
 	if (!obj) {
 		return result;
 	}
 
 	if (isFunction(iteratee)) {
-		keys = Object.keys(obj);
+		if (keys.length > 1) iteratee = optimizeCb(iteratee, keys[1]);
+		keys = allKeys(obj);
 	} else {
 		iteratee = keyInObj;
 		keys = flatten(keys);
--- test.js
+++ test.js
@@ -3,10 +3,107 @@ const pick = require('./');
 
 const isNumber = obj => toString.call(obj) === '[object Number]';
 
+const has = (obj, path) => obj != null && Object.prototype.hasOwnProperty.call(obj, path);
+
+const hasProperty = (obj, path) => {
+	if (!Array.isArray(path)) {
+		return has(obj, path);
+	}
+	const { length } = path;
+
+	for (let i = 0; i < length; i++) {
+		const key = path[i];
+
+		if (obj == null || !Object.prototype.hasOwnProperty.call(obj, key)) {
+			return false;
+		}
+		obj = obj[key];
+	}
+
+	return !!length;
+};
+
+const constant = value => () => value;
+
 describe('pick', () => {
 	it('should look through the list and return a copy of the object only containing the whitelisted properties', () => {
 		assert.deepEqual(pick({ 'a': 1, 'b': '2', 'c': 3 }, ['a', 'c']), { 'a': 1, 'c': 3 });
 		assert.deepEqual(pick({name: 'moe', age: 50, userid: 'moe1'}, 'name', 'age'), { name: 'moe', age: 50 });
 		assert.deepEqual(pick({name: 'moe', age: 50, userid: 'moe1'}, (value) => isNumber(value)), { age: 50 });
+	});
+
+	it('can restrict properties to those named', () => {
+		const result = pick({a: 1, b: 2, c: 3}, 'a', 'c');
+
+		assert.deepEqual(result, {a: 1, c: 3});
+	});
+
+	it('can restrict properties to those named in an array', () => {
+		const result = pick({a: 1, b: 2, c: 3}, ['b', 'c']);
+
+		assert.deepEqual(result, {b: 2, c: 3});
+	});
+
+	it('can restrict properties to those named in mixed args', () => {
+		const result = pick({a: 1, b: 2, c: 3}, ['a'], 'b');
+
+		assert.deepEqual(result, {a: 1, b: 2});
+	});
+
+	it('can pick numeric properties', () => {
+		const result = pick(['a', 'b'], 1);
+
+		assert.deepEqual(result, {1: 'b'});
+	});
+
+	it('should handle null/undefined', () => {
+		[null, void 0].forEach(val => {
+			assert.deepEqual(pick(val, 'hasOwnProperty'), {});
+			assert.deepEqual(pick(val, constant(true)), {});
+		});
+	});
+
+	it('can iterate primitives', () => {
+		assert.deepEqual(pick(5, 'toString', 'b'), {toString: Number.prototype.toString});
+	});
+
+	it('can accept a predicate and context', () => {
+		const data = {a: 1, b: 2, c: 3};
+		const callback = function(value, key, object) {	// eslint-disable-line
+			assert.strictEqual(key, {1: 'a', 2: 'b', 3: 'c'}[value]);
+			assert.strictEqual(object, data);
+
+			return value !== this.value;
+		};
+		const result = pick(data, callback, {value: 2});
+
+		assert.deepEqual(result, {a: 1, c: 3});
+	});
+
+	it('should include prototype props', () => {
+		const Obj = function() {};	// eslint-disable-line
+
+		Obj.prototype = {a: 1, b: 2, c: 3};
+		const instance = new Obj();
+
+		assert.deepEqual(pick(instance, 'a', 'c'), {a: 1, c: 3});
+
+		const data = {a: 1, b: 2, c: 3};
+
+		assert.deepEqual(pick(data, function(val, key) {
+			return this[key] === 3 && this === instance;
+		}, instance), {c: 3});
+	});
+
+	it('does not set own property if property not in object', () => {
+		assert.ok(!hasProperty(pick({}, 'foo'), 'foo'));
+	});
+
+	it('passes same object as third parameter of iteratee', () => {
+		const data = {a: 1, b: 2, c: 3};
+
+		pick(data, (value, key, obj) => {
+			assert.strictEqual(obj, data);
+		});
 	});
 });
